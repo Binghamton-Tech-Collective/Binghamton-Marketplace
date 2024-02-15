849105e73e02ae04a218d1d6b34f89dba5da587a
Harsh Vaghani <harshvaghani98@gmail.com>
2024-02-15 17:05:31 -0500

    Reverting the product view model to edited

--- lib/src/models/view/product.dart
+++ lib/src/models/view/product.dart
@@ -1,20 +1,55 @@
-import "../model.dart";
+import "package:btc_market/data.dart";
+import "package:btc_market/models.dart";
 
-/// The view model for the product page.
-class ProductViewModel extends ViewModel  {
-  /// Product name
-  late String productName;
+/// This view model includes the logic required for the Product View
 
-  /// The price
-  late double productPrice;
+class ProductViewModel extends ViewModel {
+  /// Stores the information about the Product
+  late final Product product;
 
-  @override
+  /// Stores the information about the Seller
+  late final SellerProfile sellerProfile;
+
+  /// Storing the list of all reviews for this particular seller
+  late final List<Review> reviews;
+
+  /// Stores the id of the product if received
+  ProductID? id;
+
+  /// Returns the average rating of the Seller
+  double get averageRating => calculateAverageRating(reviews);
+}
+
+
+
+
+/**
+ * Reference Code
+ * class ProductViewModel {
+  late final Product product;
+  late final SellerProfile profile;
+  late final List<Review> reviews;
+  final ProductID? id;
+  
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  set isLoading(bool value) {
+    _isLoading = value;
+//     notifyListeners();
+  }
+  
+  ProductViewModel.fromProduct(this.product) : 
+    id = null;
+  
+  ProductViewModel.fromID(this.id);
+  
   Future<void> init() async {
     isLoading = true;
-    await Future<void>.delayed(const Duration(seconds: 2));
-    productName = "Product Title";
-    productPrice = 49.99;
-    notifyListeners();
+    if (id != null) {
+      product = await getProduct(id);
+    }
+    profile = await getSellerProfile();
+    reviews = await ...
     isLoading = false;
   }
-}
+ */
