39df24eb8d8c3adcc71d186ac5b10bd0e9c759c3
Harsh Vaghani <harshvaghani98@gmail.com>
2024-02-15 18:37:52 -0500

    Adding the init function

--- lib/src/models/view/product.dart
+++ lib/src/models/view/product.dart
@@ -1,27 +1,33 @@
 import "package:btc_market/data.dart";
-
 import "../model.dart";
 
 /// The view model for the product page.
 class ProductViewModel extends ViewModel {
+  /// Product object
   late final Product product;
+
+  /// The id of the product
+  final ProductID id;
 
   /// Stores the information about the Seller
   late final SellerProfile sellerProfile;
 
   /// Storing the list of all reviews for this particular seller
-  late final List<Review> reviews;  
+  late final List<Review> reviews;
 
-  int? get averageRating => reviews.isEmpty ? null : calculateAverageRating(reviews);
-
-  final ProductID id;
+  /// Constructor to initialize the product
   ProductViewModel(this.id);
+
+  /// Calculates the average rating of the product
+  int? get averageRating =>
+      reviews.isEmpty ? null : calculateAverageRating(reviews);
 
   @override
   Future<void> init() async {
     isLoading = true;
-    // Download all the needed data
-
+    // this.product = getProduct(this.id);
+    // sellerProfile = await getSellerProfile(this.product.sellerID);
+    // reviews = await getReviews(this.id);
     isLoading = false;
   }
 }
