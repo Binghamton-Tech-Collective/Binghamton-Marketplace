a57b72caa3684a70b73a5da1549d14c4b20aec13
Harsh Vaghani <harshvaghani98@gmail.com>
2024-02-15 17:17:08 -0500

    Fixing analyze errors

--- lib/src/data/review.dart
+++ lib/src/data/review.dart
@@ -1,23 +1,31 @@
 import "types.dart";
 
 /// A review about a specific product or just a seller themselves.
-class Review { 
+class Review {
   /// This review's unique Review ID.
   final ReviewID id;
-  /// The author's unique User ID. 
+
+  /// The author's unique User ID.
   final UserID authorID;
+
   /// The unique Product ID of the product this is reviewing, if any.
   final ProductID? productID;
+
   /// The unique Seller ID of the seller this review is about.
   final SellerID sellerID;
-  /// The name of the user who created this review. 
+
+  /// The name of the user who created this review.
   final String authorName;
+
   /// The date and time this review was created.
   final DateTime dateTime;
+
   /// The number of stars the reviewer gave.
   final int stars;
+
   /// The title of the review.
   final String title;
+
   /// The body of the review.
   final String body;
 
@@ -27,44 +35,44 @@ class Review {
     required this.authorID,
     required this.sellerID,
     required this.authorName,
-    required this.dateTime, 
-    required this.stars, 
+    required this.dateTime,
+    required this.stars,
     required this.title,
     required this.body,
     this.productID,
   });
 
   /// Creates a new Review object from a JSON object.
-  Review.fromJson(Json json) : 
-    id = json["id"],
-    authorID = json["authorID"],
-    sellerID = json["sellerID"],
-    authorName = json["authorName"],
-    dateTime = DateTime.parse(json["dateTime"]), 
-    stars = json["stars"], 
-    title = json["title"],
-    body = json["body"],
-    productID = json["productID"];
+  Review.fromJson(Json json)
+      : id = json["id"],
+        authorID = json["authorID"],
+        sellerID = json["sellerID"],
+        authorName = json["authorName"],
+        dateTime = DateTime.parse(json["dateTime"]),
+        stars = json["stars"],
+        title = json["title"],
+        body = json["body"],
+        productID = json["productID"];
 
   /// Convert this Message to its JSON representation
   Json toJson() => {
-    "id": id,
-    "authorID": authorID,
-    "sellerID": sellerID,
-    "authorName": authorName,
-    "dateTime": dateTime.toIso8601String(), 
-    "stars": stars, 
-    "title": title,
-    "body": body,
-    "productID": productID,
-  };
+        "id": id,
+        "authorID": authorID,
+        "sellerID": sellerID,
+        "authorName": authorName,
+        "dateTime": dateTime.toIso8601String(),
+        "stars": stars,
+        "title": title,
+        "body": body,
+        "productID": productID,
+      };
 }
 
 /// Calculates the average rating out of all the seller's reviews.
-int calculateAverageRating(List<Review> reviews) { 
+double calculateAverageRating(List<Review> reviews) {
   var rating = 0;
   for (final review in reviews) {
     rating += review.stars;
   }
-  return rating ~/ reviews.length;
+  return rating / reviews.length;
 }
