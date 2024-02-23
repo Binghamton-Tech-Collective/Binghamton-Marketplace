a369e702a1d981441cef0211446b908843d94bd2
Levi Lesches <levilesches@gmail.com>
2024-02-23 01:34:28 -0500

    Created SellerProfileEditor

--- lib/pages.dart
+++ lib/pages.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 
+import "src/pages/editors/seller_profile.dart";
+
 import "src/pages/notifications.dart";
 import "src/pages/profile.dart";
 import "src/pages/shell.dart";
@@ -10,11 +12,15 @@ class Routes {
   /// The products route.
   static const products = "/products";
   /// The profile route.
-  static const profile = "/profile";
+  static const sellerProfile = "/seller-profile";
   /// The notifications route.
   static const notifications = "/notifications";
   /// The messages route.
   static const messages = "/messages";
+  /// The route to create a new seller profile.
+  /// 
+  /// This isn't the actual route used in the URL, just in the code.
+  static const createSellerProfile = "_create_seller_profile";
 }
 
 /// The [GoRouter] that controls the routing logic for the app.
@@ -25,11 +31,11 @@ final GoRouter router = GoRouter(
       builder: (context, state, shell) => ShellPage(shell),
       branches: [
         StatefulShellBranch(routes: [
-            GoRoute(
-              path: Routes.products,
-              name: Routes.products,
-              builder: (context, state) => NotificationsPage(),
-            ),
+          GoRoute(
+            path: Routes.products,
+            name: Routes.products,
+            builder: (context, state) => NotificationsPage(),
+          ),
         ],),
         StatefulShellBranch(routes: [
           GoRoute(
@@ -40,9 +46,21 @@ final GoRouter router = GoRouter(
         ],),
         StatefulShellBranch(routes: [
           GoRoute(
-            path: Routes.profile,
-            name: Routes.profile,
+            path: Routes.sellerProfile,
+            name: Routes.sellerProfile,
             builder: (context, state) => ProfilePage(),
+            routes: [
+              GoRoute(
+                path: "create",
+                name: Routes.createSellerProfile,
+                builder: (context, state) => SellerProfileEditor(),
+              ),
+              // Uncomment this to enable users to edit their profile
+              // GoRoute(
+              //   path: ":id",
+              //   builder: (context, state) => SellerProfileEditor(id: state.pathParameters["id"]!),
+              // ),
+            ],
           ),
         ],),
         StatefulShellBranch(routes: [
---
+++ lib/src/pages/editors/seller_profile.dart
@@ -0,0 +1,12 @@
+import "package:btc_market/models.dart";
+import "package:btc_market/widgets.dart";
+import "package:flutter/material.dart";
+
+class SellerProfileEditor extends ReactiveWidget<SellerProfileBuilder> {
+  @override
+  SellerProfileBuilder createModel() => SellerProfileBuilder();
+  
+  @override
+  Widget build(BuildContext context, SellerProfileBuilder model) => const Scaffold(
+  );
+}
