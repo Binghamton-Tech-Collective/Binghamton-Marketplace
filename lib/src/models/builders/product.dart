bfd90510a12056fbc2804ad6dfaa9e0152a56d69
Harsh Vaghani <harshvaghani98@gmail.com>
2024-03-05 17:49:27 -0500

    making minor changes

--- lib/src/models/builders/product.dart
+++ lib/src/models/builders/product.dart
@@ -1,108 +1,93 @@
-// import "package:btc_market/models.dart";
-// import "package:btc_market/services.dart";
-// import "package:flutter/material.dart";
-// import "package:btc_market/data.dart";
+import "package:btc_market/models.dart";
+import "package:btc_market/services.dart";
+import "package:flutter/material.dart";
+import "package:btc_market/data.dart";
 
-// extension on String {
-//   String? get nullIfEmpty => isEmpty ? null : this;
-// }
+extension on String {
+  String? get nullIfEmpty => isEmpty ? null : this;
+}
 
-// /// Class to create view model for sign up page for seller
-// class SellerProfileBuilder extends BuilderModel<SellerProfile> {
-//   /// Name of the seller
-//   final nameController = TextEditingController();
+/// Class to create view model for sign up page for seller
+class ProductBuilder extends BuilderModel<Product> {
 
-//   /// Description of the seller
-//   final bioController = TextEditingController();
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final priceController = TextEditingController();
+  // final categoriesController = TextEditingController();
+  final quantityController = TextEditingController();
+  final titleController = TextEditingController();
 
-//   /// Phone number of the seller
-//   final phoneNumberController = TextEditingController();
+  /// Fetching the seller ID.
+  late final SellerID sellerID;
 
-//   /// Tiktok username of the seller
-//   final tikTokController = TextEditingController();
+  /// Fetching the email address of the seller
+  String get email => services.auth.user!.email!;
 
-//   /// Instagram username of the seller
-//   final instagramController = TextEditingController();
+  /// Fetching User ID of the seller
+  late final UserID userID;
 
-//   /// Twitter username of the seller
-//   final twitterController = TextEditingController();
+  /// URL of the profile image
+  String? imageUrl;
 
-//   /// Linkedin username of the seller
-//   final linkedinController = TextEditingController();
+  @override
+  Future<void> init() async {
+    isLoading = true;
+    sellerID = services.database.sellers.newID;
+    userID = models.user.userProfile!.id;
+    isLoading = false;
+  }
 
-//   /// Fetching the seller ID.
-//   late final SellerID sellerID;
+  /// Function to build the profile from the input provided by the user
+  @override
+  SellerProfile build() => SellerProfile(
+        id: sellerID,
+        name: nameController.text,
+        userID: userID,
+        imageUrl: imageUrl!,
+        bio: bioController.text,
+        contact: ContactInfo(
+          email: email,
+          phoneNumber: phoneNumberController.text.nullIfEmpty,
+          tikTokUsername: tikTokController.text.nullIfEmpty,
+          instagramHandle: instagramController.text.nullIfEmpty,
+          twitterUsername: twitterController.text.nullIfEmpty,
+          linkedInUsername: linkedinController.text.nullIfEmpty,
+        ),
+      );
 
-//   /// Fetching the email address of the seller
-//   String get email => services.auth.user!.email!;
+  @override
+  bool get isReady =>
+      nameController.text.isNotEmpty &&
+      bioController.text.isNotEmpty &&
+      sellerID.toString().isNotEmpty &&
+      userID.toString().isNotEmpty &&
+      imageUrl != null &&
+      email.isNotEmpty;
 
-//   /// Fetching User ID of the seller
-//   late final UserID userID;
+  /// Upload the image provided by the user and set the imageURL to the link obtained
+  Future<void> uploadImage() async {
+    // Pick a file, upload to Firebase Storage, then set [imageUrl]
+    /**
+     * Package: file_picker => This function should be called here
+     * The functions will be in cloud_storage.dart and will be asynchronous
+     * Have a look at PlatformFile datatype
+     */
 
-//   /// URL of the profile image
-//   String? imageUrl;
+    // final file = await services.cloud_storage.pickFile();
+    // if (file == null) return;
+    // final bytes = file.bytes!;
+    // imageUrl = await services.cloud_storage
+    //     .uploadFile(bytes, "sellers/$sellerID/avatar");
+    // notifyListeners();
+  }
 
-//   @override
-//   Future<void> init() async {
-//     isLoading = true;
-//     sellerID = services.database.sellers.newID;
-//     userID = models.user.userProfile!.id;
-//     isLoading = false;
-//   }
-
-//   /// Function to build the profile from the input provided by the user
-//   @override
-//   SellerProfile build() => SellerProfile(
-//         id: sellerID,
-//         name: nameController.text,
-//         userID: userID,
-//         imageUrl: imageUrl!,
-//         bio: bioController.text,
-//         contact: ContactInfo(
-//           email: email,
-//           phoneNumber: phoneNumberController.text.nullIfEmpty,
-//           tikTokUsername: tikTokController.text.nullIfEmpty,
-//           instagramHandle: instagramController.text.nullIfEmpty,
-//           twitterUsername: twitterController.text.nullIfEmpty,
-//           linkedInUsername: linkedinController.text.nullIfEmpty,
-//         ),
-//       );
-
-//   @override
-//   bool get isReady =>
-//       nameController.text.isNotEmpty &&
-//       bioController.text.isNotEmpty &&
-//       sellerID.toString().isNotEmpty &&
-//       userID.toString().isNotEmpty &&
-//       imageUrl != null &&
-//       email.isNotEmpty;
-
-//   /// Upload the image provided by the user and set the imageURL to the link obtained
-//   Future<void> uploadImage() async {
-//     // Pick a file, upload to Firebase Storage, then set [imageUrl]
-//     /**
-//      * Package: file_picker => This function should be called here
-//      * The functions will be in cloud_storage.dart and will be asynchronous
-//      * Have a look at PlatformFile datatype
-//      */
-
-//     // TODO(harshvaghanii): uncomment the below code once you have the firebase functions ready, https://URL-to-issue.
-
-//     // final file = await services.cloud_storage.pickFile();
-//     // if (file == null) return;
-//     // final bytes = file.bytes!;
-//     // imageUrl = await services.cloud_storage
-//     //     .uploadFile(bytes, "sellers/$sellerID/avatar");
-//     // notifyListeners();
-//   }
-
-//   /// Saving the profile to Cloud Firestore
-//   Future<void> save() async {
-//     // Save the result of build() to Cloud Firestore.
-//     isLoading = true;
-//     final profile = build();
-//     await services.database.saveSellerProfile(profile);
-//     isLoading = false;
-//     notifyListeners();
-//   }
-// }
+  /// Saving the profile to Cloud Firestore
+  Future<void> save() async {
+    // Save the result of build() to Cloud Firestore.
+    isLoading = true;
+    final profile = build();
+    await services.database.saveSellerProfile(profile);
+    isLoading = false;
+    notifyListeners();
+  }
+}
