52a2929f5e42c75fe1d90d8cc50f2cc92f25c7a7
Harsh Vaghani <75880484+harshvaghanii@users.noreply.github.com>
2024-03-11 14:04:39 -0400

    Creating a draft PR for image upload (#21)
    Co-authored-by: Levi Lesches <levilesches@gmail.com>

--- lib/src/services/cloud_storage.dart
+++ lib/src/services/cloud_storage.dart
@@ -1,5 +1,7 @@
-import "dart:io";
+import "dart:typed_data";
 import "service.dart";
+import "package:file_picker/file_picker.dart";
+import "package:firebase_storage/firebase_storage.dart";
 
 /// A service to use CRUD operations on Google Cloud Storage for Firebase.
 class CloudStorageService extends Service {
@@ -9,19 +11,24 @@ class CloudStorageService extends Service {
   @override
   Future<void> dispose() async {}
 
-  /// Uploads the file to Google Cloud Storage.
-  Future<void> uploadFile({required File file, required String path}) async {}
-}
+  /// Returns a file picked by the user. 
+  Future<PlatformFile?> pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return null;
+    if (result.files.isEmpty) return null;
+    return result.files.first;
+  }
 
-Future<PlatformFile?> pickFile() async {
-  // Call `FilePicker.platform.pickFiles()`.
-  // See: https://pub.dev/documentation/file_picker/latest/file_picker/FilePicker/pickFiles.html
-  
-}
-
-Future<String> uploadFile(Uint8List data, String path) async {
-  // Make a Reference to the file at path
-  // Upload the data into that reference
-  // Call `Reference.putData()`
-  // See: https://pub.dev/documentation/firebase_storage/latest/firebase_storage/Reference/putData.html
+  /// Uploads data to a file in Firebase Storage.
+  Future<String?> uploadFile(Uint8List data, String path) async {
+    final rootReference = FirebaseStorage.instance.ref();
+    final directoryReference = rootReference.child(path);
+    try {
+      await directoryReference.putData(data);
+      final downloadURL = await directoryReference.getDownloadURL();
+      return downloadURL;
+    } catch (error) {
+      return null;
+    }
+  }
 }
