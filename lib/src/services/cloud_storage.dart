bf8a7bf0c828644a21a9b9e34d4cb58bfb7d0ef6
Levi Lesches <levilesches@gmail.com>
2024-03-11 14:01:26 -0400

    Cleanup

--- lib/src/services/cloud_storage.dart
+++ lib/src/services/cloud_storage.dart
@@ -11,29 +11,21 @@ class CloudStorageService extends Service {
   @override
   Future<void> dispose() async {}
 
-  /// Uploads the file to Google Cloud Storage.
-  /// I'm not sure why this function was created, so I'm uncommenting it. I thought functions with same names but different signatures should work, but it throws an error saying that the names are duplicate. Let me know if we need this function for something else.
-  // Future<void> uploadFile({required File file, required String path}) async {}
-
-  /// Pick the file that the user uploads and track if the user cancelled the upload or sucessfully uploaded the file.
-
+  /// Returns a file picked by the user. 
   Future<PlatformFile?> pickFile() async {
     final result = await FilePicker.platform.pickFiles();
-    if (result != null) {
-      return result.files.first;
-    } else {
-      return null;
-    }
+    if (result == null) return null;
+    if (result.files.isEmpty) return null;
+    return result.files.first;
   }
 
-  /// Upload the file to firebase storage
-
+  /// Uploads data to a file in Firebase Storage.
   Future<String?> uploadFile(Uint8List data, String path) async {
     final rootReference = FirebaseStorage.instance.ref();
     final directoryReference = rootReference.child(path);
     try {
       await directoryReference.putData(data);
-      final downloadURL = directoryReference.getDownloadURL();
+      final downloadURL = await directoryReference.getDownloadURL();
       return downloadURL;
     } catch (error) {
       return null;
