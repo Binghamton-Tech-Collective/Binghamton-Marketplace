dab6a38d748f5016fd2e66877dbee62d3d9c9b2e
Harsh Vaghani <harshvaghani98@gmail.com>
2024-03-14 17:49:57 -0400

    working on image widget

--- lib/src/pages/editors/product.dart
+++ lib/src/pages/editors/product.dart
@@ -28,6 +28,81 @@ class ProductEditor extends ReactiveWidget<ProductBuilder> {
                 hint: "Item Price",
                 controller: model.priceController,
               ),
+              //             Enter the image upload widget here
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Text(
+                      "Upload Photos",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        try {
+                          await model.uploadImage(0);
+                          
+                        }catch(error) {
+                          //TODO: Check why we're not able to access the errorText variable
+                          // errorText = "Error uploading the images!";
+                        }
+                      },
+                      child: Container(
+                        color: Colors.redAccent,
+                        height: 250,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    print("clicked card 1");
+                                  },
+                                  child: ImageUploader(
+                                    color: Colors.deepPurpleAccent,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    print("clicked card 2");
+                                  },
+                                  child: ImageUploader(
+                                    color: Colors.yellowAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    print("clicked card 3");
+                                  },
+                                  child: ImageUploader(
+                                    color: Colors.blueAccent,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    print("clicked card 4");
+                                  },
+                                  child: ImageUploader(
+                                    color: Colors.greenAccent,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
@@ -161,6 +236,52 @@ class InputContainer extends ReactiveWidget<ProductBuilder> {
         ),
       );
 }
+
+/// Reusable image uploader widget
+
+class ImageUploader extends ReactiveWidget<ProductBuilder> {
+  /// Background color of the card
+  final Color color;
+
+  /// The index of the widget to fetch the image from
+  final int? index;
+
+  /// Link to the image
+  final String? imageURL;
+
+  /// Constructor to initialize the widget
+  const ImageUploader({required this.color, this.imageURL, this.index});
+
+  @override
+  ProductBuilder createModel() => ProductBuilder();
+
+  @override
+  Widget build(BuildContext context, ProductBuilder model) => Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: model.imageUrls != null &&
+                  index != null &&
+                  index! >= 0 &&
+                  model.imageUrls!.length > index!
+              ? Image.network(
+                  model.imageUrls[index!],
+                  fit: BoxFit.cover,
+                )
+              : const Text(
+                  "Upload an Image!",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+        ),
+      );
+}
+
 // The main function is the starting point for all our Apps
 // The Material app is the parent of all the widgets that we've used in our App
 
@@ -249,7 +370,72 @@ class InputContainer extends ReactiveWidget<ProductBuilder> {
 //                 ],
 //               ),
 //             ),
-//             //Enter the image upload widget here
+//             Enter the image upload widget here
+            // Padding(
+            //     padding: EdgeInsets.all(20),
+            //     child: Column(children: [
+            //       Text(
+            //         "Upload Photos",
+            //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            //       ),
+            //       GestureDetector(
+            //         onTap: () async {
+            //           print("clicked");
+                      // Call the uploadImage function from the model
+                    },
+//                     child: Container(
+//                       color: Colors.redAccent,
+//                       height: 250,
+//                       child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                         children: [
+//                           Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                             children: [
+//                               GestureDetector(
+//                                 onTap: () {
+//                                   print("clicked card 1");
+//                                 },
+//                                 child: ReusableCard(
+//                                   color: Colors.deepPurpleAccent,
+//                                 ),
+//                               ),
+//                               GestureDetector(
+//                                 onTap: () {
+//                                   print("clicked card 2");
+//                                 },
+//                                 child: ReusableCard(
+//                                   color: Colors.yellowAccent,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                           Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                             children: [
+//                               GestureDetector(
+//                                 onTap: () {
+//                                   print("clicked card 3");
+//                                 },
+//                                 child: ReusableCard(
+//                                   color: Colors.blueAccent,
+//                                 ),
+//                               ),
+//                               GestureDetector(
+//                                 onTap: () {
+//                                   print("clicked card 4");
+//                                 },
+//                                 child: ReusableCard(
+//                                   color: Colors.greenAccent,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   ),
+//                 ],),),
 //             Padding(
 //               padding: EdgeInsets.all(20),
 //               child: Column(
@@ -260,38 +446,40 @@ class InputContainer extends ReactiveWidget<ProductBuilder> {
 //                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
 //                   ),
 //                   Container(
-//                       padding: EdgeInsets.all(6.0),
-//                       decoration: BoxDecoration(
-//                         border: Border.all(color: Colors.grey, width: 1),
-//                         borderRadius: BorderRadius.circular(15),
+//                     padding: EdgeInsets.all(6.0),
+//                     decoration: BoxDecoration(
+//                       border: Border.all(color: Colors.grey, width: 1),
+//                       borderRadius: BorderRadius.circular(15),
+//                     ),
+//                     child: DropdownButton(
+//                       items: listItems.map(
+//                         (valueItem) {
+//                           return DropdownMenuItem(
+//                             value: valueItem,
+//                             child: Text(valueItem),
+//                           );
+//                         },
+//                       ).toList(),
+//                       hint: Text("Condition"),
+//                       dropdownColor: Colors.white,
+//                       icon: Icon(Icons.arrow_drop_down),
+//                       iconSize: 36,
+//                       isExpanded: true,
+//                       underline: SizedBox(),
+//                       style: TextStyle(
+//                         color: Colors.black,
+//                         fontSize: 22,
 //                       ),
-//                       child: DropdownButton(
-//                           items: listItems.map(
-//                             (valueItem) {
-//                               return DropdownMenuItem(
-//                                 value: valueItem,
-//                                 child: Text(valueItem),
-//                               );
-//                             },
-//                           ).toList(),
-//                           hint: Text("Condition"),
-//                           dropdownColor: Colors.white,
-//                           icon: Icon(Icons.arrow_drop_down),
-//                           iconSize: 36,
-//                           isExpanded: true,
-//                           underline: SizedBox(),
-//                           style: TextStyle(
-//                             color: Colors.black,
-//                             fontSize: 22,
-//                           ),
-//                           value: valueChoose,
-//                           onChanged: (newValue) {
-//                             setState(
-//                               () {
-//                                 valueChoose = newValue;
-//                               },
-//                             );
-//                           }))
+//                       value: valueChoose,
+//                       onChanged: (newValue) {
+//                         setState(
+//                           () {
+//                             valueChoose = newValue;
+//                           },
+//                         );
+//                       },
+//                     ),
+//                   ),
 //                 ],
 //               ),
 //             ),
@@ -353,6 +541,38 @@ class InputContainer extends ReactiveWidget<ProductBuilder> {
 //           ],
 //         ),
 //       ),
+//     );
+//   }
+// }
+
+// @immutable
+// class ReusableCard extends StatelessWidget {
+//   final Color color;
+//   final String? imageURL;
+//   const ReusableCard({this.imageURL, required this.color});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 100.0,
+//       height: 100.0,
+// //       margin: const EdgeInsets.all(15.0),
+//       decoration: BoxDecoration(
+//         color: color,
+//         borderRadius: BorderRadius.circular(10.0),
+//       ),
+//       child: Center(
+//           child: this.imageURL != null
+//               ? Image.network(
+//                   imageURL!,
+//                   fit: BoxFit.cover,
+//                 )
+//               : Text(
+//                   "Upload an Image!",
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                   ),
+//                 ),),
 //     );
 //   }
 // }
