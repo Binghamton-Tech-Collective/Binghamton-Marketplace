67ca08af035b04f090ee7470be9eb8dcd637975b
Samruddhi Deshpande <sdeshp11@binghamton.edu>
2024-03-24 16:05:36 -0400

    Added SliverAppBar

--- lib/src/pages/products.dart
+++ lib/src/pages/products.dart
@@ -2,7 +2,7 @@ import "package:btc_market/main.dart";
 import "package:btc_market/src/data/notification.dart";
 import "package:btc_market/src/models/view/products.dart";
 import "package:btc_market/src/pages/notifications.dart";
-import "package:btc_market/src/pages/profile.dart";
+import "package:btc_market/src/pages/seller_profile.dart";
 import "package:flutter/material.dart";
 import "package:btc_market/models.dart";
 import "package:btc_market/widgets.dart";
@@ -14,10 +14,37 @@ class ProductsPage extends ReactiveWidget<ProductsViewModel> {
 
   @override
   Widget build(BuildContext context, ProductsViewModel model) => Scaffold(
-    appBar: AppBar(
-      title: const Text ('Home'),
-      backgroundColor: darkGreen,
-    )
-      
+
+  body: Center(child: model.isLoading
+      ? const CircularProgressIndicator()
+      : CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            collapsedHeight: 60,
+            pinned: true,
+            backgroundColor: Colors.white,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      height: 150,
+                      color: const Color.fromARGB(255, 0, 65, 44),
+                      alignment: const Alignment(0.75, 1),
+                      child: Text(
+                        "Home",
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                    )
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+        ),
+    ),
   );
 }
