a332394c31f5d0ebfb26074184ed5a3129f1d703
Harsh Vaghani <harshvaghani98@gmail.com>
2024-03-25 21:48:33 -0400

    modifying the product widget, product data model and working on search bar for home page

--- lib/src/pages/products.dart
+++ lib/src/pages/products.dart
@@ -1,7 +1,7 @@
+import "package:btc_market/main.dart";
 import "package:btc_market/models.dart";
 import "package:btc_market/src/widgets/generic/reactive_widget.dart";
 import "package:flutter/material.dart";
-
 
 /// The home page that lists all the products
 
@@ -10,7 +10,62 @@ class ProductsPage extends ReactiveWidget<ProductsViewModel> {
   ProductsViewModel createModel() => ProductsViewModel();
 
   @override
-  Widget build(BuildContext context, ProductsViewModel model) => const Scaffold(
-        body: Text("This is the homepage!"),
+  Widget build(BuildContext context, ProductsViewModel model) {
+    final productName =
+        "${"A new name which is so long that itksdfjsadk sduhfksad".substring(0, 10)}...";
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: darkGreen,
+        title: const Center(
+          child: Text(
+            "Home",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(16),
+            child: SearchBar(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// SearchBar widget
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: darkGreen, // Background color of the search bar
+        ),
+        child: TextField(
+          style: const TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            hintText: "Search here", // Hint text
+            hintStyle: const TextStyle(
+              color: Colors.white,
+            ),
+            prefixIcon: GestureDetector(
+              onTap: () {},
+              child: const Icon(Icons.search),
+            ), // Search icon
+            border: InputBorder.none, // Remove the default border
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 14,
+            ), // Padding inside the TextField
+          ),
+        ),
       );
 }
