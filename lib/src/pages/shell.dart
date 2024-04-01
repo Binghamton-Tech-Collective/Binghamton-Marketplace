bf78f0467eca210ee52be92a44ba65a83ef8ae41
Harsh Vaghani <harshvaghani98@gmail.com>
2024-04-01 12:44:47 -0400

    adding notched floating widget button

--- lib/src/pages/shell.dart
+++ lib/src/pages/shell.dart
@@ -1,3 +1,5 @@
+import "package:btc_market/models.dart";
+import "package:btc_market/pages.dart";
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 
@@ -11,27 +13,43 @@ class ShellPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     body: shell,
-    bottomNavigationBar: NavigationBar(
-      selectedIndex: shell.currentIndex,
-      onDestinationSelected: shell.goBranch,
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.storefront),
-          label: "Products",
+    floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if(models.user.sellerProfiles.isNotEmpty) {
+              router.go("/products/create");
+            } else {
+              
+            }
+          },
+          tooltip: "Add Product",
+          backgroundColor: Colors.grey[200],
+          child: const Icon(Icons.add),
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    bottomNavigationBar: BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      child: NavigationBar(
+        selectedIndex: shell.currentIndex,
+        onDestinationSelected: shell.goBranch,
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.storefront),
+            label: "Products",
+            ),
+          NavigationDestination(
+            icon: Icon(Icons.message),
+            label: "Message",
+            ),
+          NavigationDestination(
+            icon: Icon(Icons.groups),
+            label: "Sellers",
           ),
-        NavigationDestination(
-          icon: Icon(Icons.message),
-          label: "Message",
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: "My Profile",
           ),
-        NavigationDestination(
-          icon: Icon(Icons.groups),
-          label: "Sellers",
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.person),
-          label: "My Profile",
-        ),
-      ],
+        ],
+      ),
     ),
   );      
 } 
