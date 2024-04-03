b47901801b6f947b2abb479c607432d102f437e3
Levi Lesches <levilesches@gmail.com>
2024-04-03 04:37:22 -0400

    Fixed add product button

--- lib/src/pages/shell.dart
+++ lib/src/pages/shell.dart
@@ -1,5 +1,6 @@
 import "package:btc_market/models.dart";
 import "package:btc_market/pages.dart";
+import "package:btc_market/widgets.dart";
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 
@@ -13,39 +14,51 @@ class ShellPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     body: shell,
-    floatingActionButton: FloatingActionButton(
-      onPressed: () => models.user.sellerProfiles.isNotEmpty
-        ? router.go("/products/create")
-        : router.go("/sellers/no-profile"),
-      tooltip: "Add Product",
-      backgroundColor: Colors.grey[200],
-      child: const Icon(Icons.add),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    bottomNavigationBar: BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      child: NavigationBar(
-        selectedIndex: shell.currentIndex,
-        onDestinationSelected: shell.goBranch,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.storefront),
-            label: "Products",
-            ),
-          NavigationDestination(
-            icon: Icon(Icons.message),
-            label: "Message",
-            ),
-          NavigationDestination(
-            icon: Icon(Icons.groups),
-            label: "Sellers",
+    bottomNavigationBar: NavigationBar(
+      selectedIndex: shell.currentIndex,
+      onDestinationSelected: (index) => switch (index) {
+        0 || 1 => shell.goBranch(index),
+        2 => models.user.sellerProfiles.isNotEmpty
+          ? router.go("/products/create")
+          : router.go("/sellers/no-profile"),
+        3 || 4 => shell.goBranch(index - 1),
+        _ => null,
+      },
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.storefront),
+          label: "Products",
           ),
-          NavigationDestination(
-            icon: Icon(Icons.person),
-            label: "My Profile",
+        NavigationDestination(
+          icon: Icon(Icons.message),
+          label: "Messages",
           ),
-        ],
-      ),
+        NavigationDestination(
+          icon: Icon(Icons.sell),
+          label: "Sell Something",
+          ),
+        // Column(
+        //   mainAxisSize: MainAxisSize.min,
+        //   children: [
+        //     IconButton(
+        //       onPressed: () => models.user.sellerProfiles.isNotEmpty
+        //         ? router.go("/products/create")
+        //         : router.go("/sellers/no-profile"),
+        //       tooltip: "Add Product",
+        //       icon: const Icon(Icons.sell),
+        //     ),
+        //     Text("Sell something", style: context.textTheme.labelMedium),
+        //   ],
+        // ),
+        NavigationDestination(
+          icon: Icon(Icons.groups),
+          label: "Sellers",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person),
+          label: "My Profile",
+        ),
+      ],
     ),
   );      
 } 
