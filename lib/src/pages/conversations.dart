a8a2ec2cdd9730c0aa0e719361882fe6c89c64d5
Samruddhi Deshpande <sdeshp11@binghamton.edu>
2024-04-04 17:11:46 -0400

    Placeholder Created with img error

--- lib/src/pages/conversations.dart
+++ lib/src/pages/conversations.dart
@@ -1,6 +1,9 @@
+import "package:flutter/material.dart";
+
+
 import "package:btc_market/models.dart";
 import "package:btc_market/widgets.dart";
-import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 /// The page that displays all conversation for the user
 class ConversationsPage extends ReactiveWidget<ConversationsViewModel> {
@@ -12,11 +15,29 @@ class ConversationsPage extends ReactiveWidget<ConversationsViewModel> {
     appBar: AppBar(
       title: const Text("Chats"),
     ),
-    body: ListView.builder(
+    body: model.allConversations.isNotEmpty
+    ? ListView.builder(
       itemCount: model.allConversations.length,
       itemBuilder: (context, index) => ConversationWidget(
         conversation: model.allConversations[index],
       ),
+    )
+    : Center(
+      child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Image(image: AssetImage("assets\categories\baxterInGreenhouse.jpg",),
+        ),
+        const SizedBox(height: 16,),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: const Text("Start of your conversation history!",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+          ),          
+        ),
+      ],
     ),
-  );
+    ),
+    );
 }
