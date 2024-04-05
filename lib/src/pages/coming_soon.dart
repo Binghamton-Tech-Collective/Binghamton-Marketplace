5690894af89bf56827fbf00dc465a939d2336248
Harsh Vaghani <75880484+harshvaghanii@users.noreply.github.com>
2024-04-05 03:10:16 -0400

    Implemented the Coming Soon Page! (#46)
    Added the coming soon page with an image of baxter and some text!
    This page will show up when someone visits the Profile page!
    
    ---------
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>

---
+++ lib/src/pages/coming_soon.dart
@@ -0,0 +1,33 @@
+import "package:flutter/material.dart";
+
+/// Coming Soon page for features that are a part of next phase
+class ComingSoonPage extends StatelessWidget {
+  /// Constructor for page
+  const ComingSoonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text("Coming Soon!",
+      style: TextStyle(
+        color: Colors.white,
+      ),),
+    ),
+    body: Center(child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Image(image: AssetImage("assets/bearcat/baxter.jpg",
+        ),),
+        const SizedBox(height: 16,),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: const Text(
+            "Our squad's cooking up this page!\n It's gonna drop soon, stay tuned!",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+          ),
+        ),
+      ],
+    ),),
+  );
+}
