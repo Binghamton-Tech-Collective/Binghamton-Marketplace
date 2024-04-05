458c1745e8b62ac58777bac343c96691fd6a66f7
Levi Lesches <levilesches@gmail.com>
2024-04-05 03:01:38 -0400

    Touched up code

--- lib/src/pages/coming_soon.dart
+++ lib/src/pages/coming_soon.dart
@@ -8,24 +8,26 @@ class ComingSoonPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      title: const Text("Coming Soon!!",
+      title: const Text("Coming Soon!",
       style: TextStyle(
         color: Colors.white,
       ),),
     ),
-    body: Column(
+    body: Center(child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const Image(image: AssetImage("/bearcat/baxter.jpg",
+        const Image(image: AssetImage("assets/bearcat/baxter.jpg",
         ),),
         const SizedBox(height: 16,),
         Container(
           padding: const EdgeInsets.all(16),
-          child: const Text("Our squad's cooking up this page! It's gonna drop soon, stay tuned!",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),),
+          child: const Text(
+            "Our squad's cooking up this page!\n It's gonna drop soon, stay tuned!",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+          ),
         ),
       ],
-    ),
+    ),),
   );
 }
