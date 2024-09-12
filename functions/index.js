456965dcfd10ba11c413af37c041ad6943ae7640
harshvaghanii <harshvaghani98@gmail.com>
2024-09-12 19:10:13 -0400

    refactoring the firebase cloud function file

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 const functions = require("firebase-functions/v2"); // Firebase Functions v2
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -44,7 +25,7 @@ exports.sendNotificationOnNewMessage = functions.firestore.onDocumentUpdated("co
             const buyerUID = afterData.buyerUID;
             const sellerUID = afterData.sellerUID;
 
-            // Determine the recipient (receiver)
+            // Determine the sender and the receiver
             const currentUserUID = newMessage.author;
             let receiverUID = null;
             let senderUID = null;
@@ -83,8 +64,8 @@ exports.sendNotificationOnNewMessage = functions.firestore.onDocumentUpdated("co
                 const payload = {
                     token: receiverFcmToken,
                     notification: {
-                        title: `New message from ${senderName}`, // Customize the title
-                        body: newMessage.content || "A new Message",  // The message content
+                        title: `${senderName}`,
+                        body: newMessage.content || "A new Message",
                     },
                 };
 
